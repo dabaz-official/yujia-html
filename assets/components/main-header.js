08218c65e0976e98936152afd8ff8447e9f8ebe5
DabAZ <76861837+dabaz-official@users.noreply.github.com>
2022-10-21 03:52:59 +0800

    Update some components

--- assets/components/main-header.js
+++ assets/components/main-header.js
@@ -1,15 +1,15 @@
 class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <header class="sticky top-0 z-40 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+      <header class="sticky top-0 z-40 grid grid-cols-3 bg-white/[.64] backdrop-blur-md shadow-md p-5 md:px-10">
         <!-- Left -->
-        <div class="relative flex items-center h-10 cursor-pointer my-auto">
+        <a href="/" class="relative flex items-center h-10 cursor-pointer my-auto">
           <img
             src="/assets/images/global/logos/primary-logo.svg"
             alt="予家"
             class="h-8 w-auto md:h-10"
           />
-        </div>
+        </a>
 
         <!-- Middle -->
         <div class="flex items-center md:border-2 rounded-full py-2 md:shadow-sm md:hover:shadow-md transition duration-200">
