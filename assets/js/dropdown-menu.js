13c6749527ab4ec8270a1a0a3b240f0c9f9c8f76
DabAZ <76861837+dabaz-official@users.noreply.github.com>
2022-10-21 02:18:03 +0800

    Add files via upload

--- assets/js/dropdown-menu.js
+++ assets/js/dropdown-menu.js
@@ -3,6 +3,10 @@ window.onload = function(){
   let menu = document.getElementById("menu");
 
   dropdownBtn.onclick = function(){
-    menu.style.display = "block";
+    if(menu.style.display === "none") {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
   }
 }
