7533a24a1a2083ad9914ad281449c43f4f4a1a91
DabAZ <76861837+dabaz-official@users.noreply.github.com>
2022-11-07 21:22:31 +0800

    Add files via upload

--- assets/components/original-header.js
+++ assets/components/original-header.js
@@ -1,6 +1,7 @@
 class Header extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `
+    this.innerHTML = 
+    `
       <header class="sticky top-0 z-40 grid grid-cols-3 bg-white/[.64] backdrop-blur-md shadow-md p-5 md:px-10">
         <!-- Left -->
         <a href="/" class="relative flex items-center h-10 cursor-pointer my-auto">
@@ -12,7 +13,7 @@ class Header extends HTMLElement {
         </a>
 
         <!-- Middle -->
-        <div class="flex items-center md:border-2 rounded-full py-2 md:shadow-sm md:hover:shadow-md transition duration-200">
+        <div class="flex items-center md:border-2 rounded-md py-2 md:shadow-sm md:hover:shadow-md transition duration-200">
           <input 
             class="flex-grow pl-5 outline-none bg-transparent text-sm text-gray-600 placeholder-gray-400" 
             type="text" 
@@ -29,15 +30,15 @@ class Header extends HTMLElement {
         <div class="flex items-center justify-end">
           <div class="inline w-56 text-right space-x-4 text-gray-500">
             <a href="/rent" class="text-sm font-bold hidden md:inline hover:text-gray-700 transition duration-200">
-              租房
+              租s
             </a>
             <a href="/host" class="text-sm font-bold hidden md:inline hover:text-gray-700 transition duration-200">
               发布房源
             </a>
-            <div class="relative inline-block text-left">
-              <div>
-                <button id="dropdownBtn" class="inline-flex w-full justify-center rounded-full bg-blue-500 px-2 py-2 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 transition duration-200">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke-width="1.5" stroke="currentColor" class="mb-1 mr-1 h-5 w-5 text-slate-200 hover:text-slate-100">
+            <div class="relative inline-block text-left dropdown">
+              <div class="group">
+                <button id="dropdownBtn" class="inline-flex w-full justify-center rounded-md border-2 px-2 py-2 text-sm font-medium text-white group-hover:bg-blue-600 active:ring-2 active:ring-blue-500 active:ring-opacity-75 transition duration-200">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke-width="1.5" stroke="currentColor" class="mb-1 mr-1 h-5 w-5 text-slate-800 group-hover:text-slate-100">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                   </svg>
                 </button>
@@ -58,13 +59,23 @@ class Header extends HTMLElement {
                   </div>
                   <div class="p-1">
                     <li>
-                      <a href="/login.html" class="text-gray-900 group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-blue-500 hover:text-white transition duration-200">
-                        登录
+                      <a href="/account-settings" class="text-gray-900 group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-blue-500 hover:text-white transition duration-200">
+                        账号
                       </a>
                     </li>
                     <li>
-                      <a href="/register.html" class="text-gray-900 group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-blue-500 hover:text-white transition duration-200">
-                        注册
+                      <a href="/account-settings/history.html" class="text-gray-900 group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-blue-500 hover:text-white transition duration-200">
+                        历史订单
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/account-settings/inbox.html" class="text-gray-900 group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-blue-500 hover:text-white transition duration-200">
+                        通知
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" class="text-gray-900 group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-blue-500 hover:text-white transition duration-200">
+                        退出
                       </a>
                     </li>
                   </div>
@@ -78,4 +89,4 @@ class Header extends HTMLElement {
   }
 }
 
-customElements.define('header-before-login', Header);
+customElements.define('original-header', Header);
